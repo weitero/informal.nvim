63ce7e79da161a7acd567390d8df2ed68f36e2ef
weitero <112836181+weitero@users.noreply.github.com>
2025-06-12 17:58:51 +0800

    separate config and core functions

--- lua/informal/config.lua
+++ lua/informal/config.lua
@@ -1,9 +1,13 @@
 local M = {}
-M.defaults = {
+local defaults = {
   force_block_ignore = false,
   formatters_by_ft = {
     lua = { "stylua" },
     python = { "ruff" },
   },
 }
+M.opts = {}
+function M.setup(opts)
+  M.opts = vim.tbl_deep_extend("force", {}, defaults, opts or {})
+end
 return M
---
+++ lua/informal/ignore.lua
@@ -0,0 +1,35 @@
+local config = require("informal.config")
+local pragma_comments = require("informal.pragma_comments").pragma_comments
+local utils = require("informal.utils")
+local M = {}
+function M.get_formatters()
+  local ft = vim.bo.filetype
+  local formatters = config.opts.formatters_by_ft[ft]
+  if not formatters then
+    return {}
+  end
+  return formatters
+end
+function M.add_comments()
+  if not utils.is_visual_mode() then
+    return
+  end
+  local start_line, end_line = utils.get_range()
+  local formatters = M.get_formatters()
+  if not formatters then
+    return
+  end
+  for _, formatter in ipairs(formatters) do
+    local formatter_pragma_comments = pragma_comments[formatter]
+    if start_line == end_line then
+      local comment_string = formatter_pragma_comments[1]
+      vim.api.nvim_buf_set_lines(0, start_line - 1, start_line - 1, true, { comment_string })
+    else
+      local comment_string = formatter_pragma_comments[2]
+      vim.api.nvim_buf_set_lines(0, start_line - 1, start_line - 1, true, { comment_string[1] })
+      vim.api.nvim_buf_set_lines(0, end_line + 1, end_line + 1, true, { comment_string[2] })
+    end
+  end
+end
+vim.keymap.set("v", "<leader>c", M.add_comments, { desc = "" })
+return M
