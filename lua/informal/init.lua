4572480cf76873dd1724940165f6145ccf6adf7d
weitero <112836181+weitero@users.noreply.github.com>
2026-01-08 17:59:13 +0000

    fix: correct typo in add_comments function assignment

--- lua/informal/init.lua
+++ lua/informal/init.lua
@@ -2,5 +2,5 @@ local config = require("informal.config")
 local ignore = require("informal.ignore")
 local M = {}
 M.setup = config.setup
-M.add_comemnts = ignore.add_comments
+M.add_comments = ignore.add_comments
 return M
